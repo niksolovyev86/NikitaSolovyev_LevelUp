89bcd3fce88b404662c695cbc83f3c1f437e27ce
Nikita Solovyev <niksolovye86@gmail.com>
2023-06-06 00:22:52 +0300

    Some CheckStyle warnings changes

--- src/main/java/ru/levelp/at/Main.java
+++ src/main/java/ru/levelp/at/Main.java
@@ -1,8 +1,14 @@
-package ru.levelp.at;
-
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
+//package ru.levelp.at;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        TicketNumberCompare ticketNumberCompare = new TicketNumberCompare();
+//
+//        if (ru.levelp.at.TicketNumberCompare.ticketNumberCompare("623516")) {
+//            System.out.printf("true");
+//        }
+//        System.out.printf("false");
+//
+//    }
+//}
